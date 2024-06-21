7ee30a5a52f8fe1f2ed57715f01f322bb207e979
Amarildo Piovesan Junior <142052228+AmarildoPJ@users.noreply.github.com>
2024-06-21 14:49:45 -0300

    Update system.ino

--- system.ino
+++ system.ino
@@ -11,9 +11,7 @@ void setup() {
     delay(10);                      // Aguarda a inicialização da porta serial
   }
   
-  // Inicializa a comunicação I2C com pinos definidos para o ESP32
   Wire.begin(19, 22);               // Configura o pino GPIO 19 como SDA e GPIO 22 como SCL
-
   if (!mpu.begin()) {               // Inicializa o sensor MPU6050
     while (1) {
       delay(10);                    // Entra em loop infinito caso o sensor não seja detectado
@@ -28,7 +26,7 @@ void setup() {
 
 void loop() {
   sensors_event_t a, g, temp;        // Declara estruturas para armazenar dados do acelerômetro, giroscópio e temperatura
-  
+
   mpu.getEvent(&a, &g, &temp);       // Captura os dados do sensor
 
   // Imprime os valores de aceleração em cada eixo (X, Y, Z) na porta serial
